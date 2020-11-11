9a5f6b5930f48bc3ecc5413a958f5055673fa052
Claudeegarcia7 <Claudeegarcia@icloud.com>
2020-11-11 16:25:16 -0600

    commit tree a015afc8fdb14d8c18e16997b457f4eb20bf4111

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,64 @@ class Song
     self.class.all << self
   end
 
+  def self.find_or_create_by_name(song_name)
+    find_by_name(song_name) || create_by_name(song_name)
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+
+
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+
+  def save
+    self.class.all << self
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find{ |song| song.name == song_name}
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name}
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    song.name =(name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name =(name.split(" - ")[0])
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    song.name =(name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name =(name.split(" - ")[0])
+    @@all << song
+    song
+  end
+
+  def self.destroy_all()
+    @@all.clear
+  end
+  
+
+
+
 end
